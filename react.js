119582ab3680a97f227cbb69c84342092ca72b35
Emmie Päivärinta <Emmie.Paivarinta@gmail.com>
2018-03-26 20:30:40 +0200

    Prefer self-closing tags in jsx

--- react.js
+++ react.js
@@ -2,6 +2,13 @@ module.exports = {
   extends: [
     './index.js',
     'plugin:react/recommended'
-  ]
+  ],
+  rules: {
+    /* react code style: jsx */
+    'react/self-closing-comp': ['warn', { /* prefer self-closing tags */
+      component: true,
+      html: true
+    }]
+  }
 };
 
